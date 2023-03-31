3e8132e0eada8c2102f5311fd79e3509839c10d0
React <3809164@gmail.com>
2023-03-31 21:02:09 +0300

    change

--- weather/src/App.js
+++ weather/src/App.js
@@ -25,52 +25,66 @@ function App() {
     }
 
     return (
-        <div className={'w-full h-screen z-[-1]'}>
-            {nav ?
-                <img className={"top-0 left-0 w-full h-screen object-cover absolute z-[-1] object-cover"} src={img1} alt={"1"}/>
-                : <img className={"top-0 left-0 w-full h-screen object-cover absolute z-[-1] object-cover"} src={img2} alt={"2"}/>}
-
-            {/*<div className={'w-full h-screen z-[-1] bg-cover px-5'} style={{backgroundImage: `url(${img})`}}>*/}
-            <div className={"text-center pt-10 md:py-20"}>
-                <input className={"w-[50%] p-2 bg-white/20 rounded-lg text-white "}
-                       value={location}
-                       onChange={event => setLocation(event.target.value)}
-                       onKeyPress={searchLocation}
-                       placeholder='Enter city'
-                       type="text"/>
+        <div className={"w-full h-screen"}>
+            <div className={"h-full w-full absolute z-[-1]"}>
+                {nav ? <img className={"absolute h-full w-full object-cover"} src={img1}/>
+                    : <img className={"absolute h-full w-full object-cover"} src={img2}/>}
             </div>
-            <div className={"max-w-[700px] flex flex-col m-auto justify-between text-white z-20 relative"}>
-                <div className={"flex flex-col text-center items-center justify-center my-8"}>
-                    <div className={"text-white z-20 font-bold text-2xl"}>
-                        <p className={" p-8 text-2xl md:text-4xl uppercase"}>{data.name}</p>
-                    </div>
-                    <div className={""}>
-                        {data.main ? <h1 className={"text-[100px] md:text-[136px]"}>{(data.main.temp.toFixed() - 273)}°</h1> : null}
-                    </div>
-                    <div>
-                        {data.weather ? <p className={'text-2xl text-bold my-5 uppercase'}>{data.weather[0].main}</p> : null}
-                    </div>
+            <div className={"max-w-[700px] h-full grid grid-cols-1 grid-rows-4 text-white text-center m-auto p-4"}>
+                <div className={'w-[70%] m-auto text-white'}>
+                    <input className={"w-full rounded-2xl px-4 py-1 bg-white/10 text-white"}
+                           value={location}
+                           onChange={event => setLocation(event.target.value)}
+                           onKeyPress={searchLocation}
+                           placeholder='Enter city'
+                           type="text"/>
                 </div>
-                {data.name !== undefined &&
-                    <div className={"flex justify-evenly text-center w-full bg-white/10 opacity-100 rounded-lg "}>
-                        <div className={"z-20"}>
-                            {data.main ?
-                                <p className='bold text-white'>{data.main.feels_like.toFixed() - 273}°</p> : null}
-                            <p>Feels Like</p>
-                        </div>
-                        <div className="humidity">
-                            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-                            <p>Humidity</p>
-                        </div>
-                        <div className="wind">
-                            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-                            <p>Wind Speed</p>
-                        </div>
+                <div className={"max-h-[50%] flex justify-evenly items-center md:text-amber-700 lg:text-amber-900"}>
+                    <div className={""}>
+                        {nav ? <p className={"text-4xl text-white"}>{data.name}</p>
+                            : <p className={"text-4xl text-zinc-700"}>{data.name}</p>}
+
                     </div>
-                }
-                <div className={"max-w-[500px] flex m-auto text-2xl mt-4"}>
+                    {nav ?  <div className={"text-[70px] text-white lg:text-[200px]"}>
+                        {data.main ?
+                            <h1 className={""}>{(data.main.temp.toFixed() - 273)}°</h1> : null}
+                    </div>
+                    :  <div className={"text-[70px] text-zinc-700 md:text-[200px]"}>
+                            {data.main ?
+                                <h1 className={""}>{(data.main.temp.toFixed() - 273)}°</h1> : null}
+                        </div>}
+                    {nav ?  <div className={"text-4xl text-white"}>
+                        {data.weather ?
+                            <p className={' text-4xl'}>{data.weather[0].main}</p> : null}
+                    </div>
+                        :  <div className={"text-4xl text-zinc-700"}>
+                        {data.weather ?
+                            <p className={' text-4xl'}>{data.weather[0].main}</p> : null}
+                    </div>}
+                </div>
+                <div className={""}>
+                    {data.name !== undefined &&
+                        <div
+                            className={"flex justify-evenly items-center bg-white/10 rounded-xl"}>
+                            <div className={"z-20"}>
+                                {data.main ?
+                                    <p className=''>{data.main.feels_like.toFixed() - 273}°</p> : null}
+                                <p>Feels Like</p>
+                            </div>
+                            <div className="">
+                                {data.main ? <p className=''>{data.main.humidity}%</p> : null}
+                                <p>Humidity</p>
+                            </div>
+                            <div className="">
+                                {data.wind ? <p className=''>{data.wind.speed.toFixed()} MPH</p> : null}
+                                <p>Wind Speed</p>
+                            </div>
+                        </div>
+                    }
+                </div>
+                <div className={"flex justify-evenly items-center lg:text-amber-900"}>
                     <button onClick={handleBg}
-                            className={"w-full border text-white px-10 py-2 font-bold hover:text-cyan-500 rounded-2xl"}>Change
+                            className={"bg-white/30 px-6 py-2 rounded-xl"}>Change
                         Background
                     </button>
                 </div>
